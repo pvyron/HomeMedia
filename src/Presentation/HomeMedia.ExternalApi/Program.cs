4bbe149dec0ff81c19fee721935ed98385841e39
pvyron <pvyron@gmail.com>
2023-05-04 23:42:56 +0300

    add privacy policy

--- src/Presentation/HomeMedia.ExternalApi/Program.cs
+++ src/Presentation/HomeMedia.ExternalApi/Program.cs
@@ -40,6 +40,8 @@ var errors = new List<string>() { "I am working" };
 
 app.MapGet("/", () => Results.Ok(errors));
 
+app.MapGet("/privacy", () => Results.Ok(PrivacyPolicy.POLICY));
+
 app.MapPost("api/torrents/search", async ([FromBody] TorrentsSearchRequestModel requestModel, [FromServices] ISender mediator, CancellationToken cancellationToken) =>
 {
     try
